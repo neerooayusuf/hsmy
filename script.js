db715c599779dab1dcae0f6847254634769ae0ab
neerooayusuf <40438675+neerooayusuf@users.noreply.github.com>
2023-01-13 01:08:18 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,41 @@
 (function () {
-  const second = 1000,
-        minute = second * 60,
-        hour = minute * 60,
-        day = hour * 24;
-
-  //I'm adding this section so I don't have to keep updating this pen every year :-)
-  //remove this if you don't need it
-  let today = new Date(),
-      dd = String(today.getDate()).padStart(2, "0"),
-      mm = String(today.getMonth() + 1).padStart(2, "0"),
-      yyyy = today.getFullYear(),
-      nextYear = yyyy + 1,
-      dayMonth = "02/24/",
-      birthday = dayMonth + yyyy;
   
-  today = mm + "/" + dd + "/" + yyyy;
-  if (today > birthday) {
-    birthday = dayMonth + nextYear;
+  var _end_year = 2023;
+  var _end_month = 2;
+  var _end_date = 24;
+  var _end_hour = 2;
+  var _end_minute = 10;
+  var _end_seconds = 0;
+
+  var end = new Date(Date.UTC(_end_year, _end_month - 1, _end_date, _end_hour, _end_minute, _end_seconds));
+
+  var _second = 1000;
+  var _minute = _second * 60;
+  var _hour = _minute * 60;
+  var _day = _hour * 24;
+  //var _month = _day * 30;
+  var timer;
+
+  function showRemaining() {
+    var now = new Date();
+    var distance = end - now;
+    if (distance < 0) {
+      document.getElementById("headline").innerText = "I have landed in Kuala Lumpur!!!";
+      document.getElementById("countdown").style.display = "none";
+      document.getElementById("content").style.display = "block";
+      clearInterval(x);
+    }
+    //var months = Math.floor(distance / _month);
+    var days = Math.floor(distance / _day);
+    var hours = Math.floor((distance % _day) / _hour);
+    var minutes = Math.floor((distance % _hour) / _minute);
+    var seconds = Math.floor((distance % _minute) / _second);
+
+          document.getElementById("days").innerText = days,
+          document.getElementById("hours").innerText = hours,
+          document.getElementById("minutes").innerText = minutes,
+          document.getElementById("seconds").innerText = seconds;
+
   }
-  //end
-  
-  const countDown = new Date(birthday).getTime(),
-      x = setInterval(function() {    
-
-        const now = new Date().getTime(),
-              distance = countDown - now;
-
-        document.getElementById("days").innerText = Math.floor(distance / (day)),
-          document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-          document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-          document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
-
-        //do something later when date is reached
-        if (distance < 0) {
-          document.getElementById("headline").innerText = "It's my birthday!";
-          document.getElementById("countdown").style.display = "none";
-          document.getElementById("content").style.display = "block";
-          clearInterval(x);
-        }
-        //seconds
-      }, 0)
-  }());
+  timer = setInterval(showRemaining, 1000);
+}());
