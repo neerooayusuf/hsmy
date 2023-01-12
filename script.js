85eb3926a8573f081e1dde22c17e9b46afc6bb0a
neerooayusuf <40438675+neerooayusuf@users.noreply.github.com>
2023-01-13 00:06:12 +0400

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@
       mm = String(today.getMonth() + 1).padStart(2, "0"),
       yyyy = today.getFullYear(),
       nextYear = yyyy + 1,
-      dayMonth = "09/30/",
+      dayMonth = "02/24/",
       birthday = dayMonth + yyyy;
   
   today = mm + "/" + dd + "/" + yyyy;
