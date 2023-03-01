7a156a3859e1871a38f39eed4b7b9e5e4b246447
Muhammad Yusuf Neerooa <yusuf@cloudmails.apu.edu.my>
2023-03-01 22:45:35 +0800

    inflight option

--- script.js
+++ script.js
@@ -1,41 +1,65 @@
 (function () {
-  
-  var _end_year = 2023;
-  var _end_month = 3;
-  var _end_date = 2;
-  var _end_hour = 2;
-  var _end_minute = 10;
-  var _end_seconds = 0;
 
-  var end = new Date(Date.UTC(_end_year, _end_month - 1, _end_date, _end_hour, _end_minute, _end_seconds));
+    var _end_year = 2023;
+    var _end_month = 3;
+    var _end_date = 2;
+    var _end_hour = 2;
+    var _end_minute = 10;
+    var _end_seconds = 0;
 
-  var _second = 1000;
-  var _minute = _second * 60;
-  var _hour = _minute * 60;
-  var _day = _hour * 24;
-  //var _month = _day * 30;
-  var timer;
+    var _flightDuration_day = 0;
+    var _flightDuration_hour = 7;
+    var _flightDuration_minute = 10;
+    var _flightDuration_seconds = 0;
 
-  function showRemaining() {
-    var now = new Date();
-    var distance = end - now;
-    if (distance < 0) {
-      document.getElementById("headline").innerText = "Halima Saadiyah\nhas landed in\nKuala Lumpur!!!";
-      document.getElementById("countdown").style.display = "none";
-      document.getElementById("content").style.display = "block";
-      clearInterval(x);
+    var end = new Date(Date.UTC(_end_year, _end_month - 1, _end_date, _end_hour, _end_minute, _end_seconds));
+
+    var _second = 1000;
+    var _minute = _second * 60;
+    var _hour = _minute * 60;
+    var _day = _hour * 24;
+    //var _month = _day * 30;
+    var timer;
+
+    function showRemaining() {
+        var now = new Date();
+        var flightDuration = (_flightDuration_day * 60 * 60 * 24 + _flightDuration_hour * 60 * 60 + _flightDuration_minute * 60 + _flightDuration_seconds) * 1000;
+        var inflight = (end - flightDuration - now) ;
+        var distance = end - now;
+        console.log("flightDuration: " + flightDuration)
+        console.log("inflight: " + inflight);
+        console.log("distance: " + distance);
+        if (inflight < 0) {
+            var days = Math.floor(distance / _day);
+            var hours = Math.floor((distance % _day) / _hour);
+            var minutes = Math.floor((distance % _hour) / _minute);
+            var seconds = Math.floor((distance % _minute) / _second);
+            document.getElementById("headline").innerText = "Halima Saadiyah\nhas taken the flight to\nKuala Lumpur!!!";
+            document.getElementById("days").innerText = days,
+                document.getElementById("hours").innerText = hours,
+                document.getElementById("minutes").innerText = minutes,
+                document.getElementById("seconds").innerText = seconds;
+
+            if (distance < 0) {
+                document.getElementById("headline").innerText = "Halima Saadiyah\nhas landed in\nKuala Lumpur!!!";
+                document.getElementById("countdown").style.display = "none";
+                document.getElementById("content").style.display = "block";
+                clearInterval(x);
+            }
+        }
+        else {
+            var days = Math.floor(inflight / _day);
+            var hours = Math.floor((inflight % _day) / _hour);
+            var minutes = Math.floor((inflight % _hour) / _minute);
+            var seconds = Math.floor((inflight % _minute) / _second);
+
+            document.getElementById("days").innerText = days,
+                document.getElementById("hours").innerText = hours,
+                document.getElementById("minutes").innerText = minutes,
+                document.getElementById("seconds").innerText = seconds;
+        }
+        //var months = Math.floor(distance / _month);
+        
     }
-    //var months = Math.floor(distance / _month);
-    var days = Math.floor(distance / _day);
-    var hours = Math.floor((distance % _day) / _hour);
-    var minutes = Math.floor((distance % _hour) / _minute);
-    var seconds = Math.floor((distance % _minute) / _second);
-
-          document.getElementById("days").innerText = days,
-          document.getElementById("hours").innerText = hours,
-          document.getElementById("minutes").innerText = minutes,
-          document.getElementById("seconds").innerText = seconds;
-
-  }
-  timer = setInterval(showRemaining, 1000);
+    timer = setInterval(showRemaining, 1000);
 }());
